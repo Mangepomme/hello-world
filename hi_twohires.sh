139b8df159acaa60afc0d4faf9b502f157f2c7db
Louise Flick <louise.flick@telecom-paris.fr>
2020-07-21 17:16:35 +0200

    delete trash

--- hi_twohires.sh
+++
@@ -1,9 +0,0 @@
-./../setsched G-RES ;
-sleep 1 ;
-./../resctl -n 100 -c 1 -t table-driven -m 200 '[20,70]' ;
-./../resctl -n 101 -c 1 -t table-driven -m 200 '[130,190]' ;
-./../resctl -n 101 -c 0 -t table-driven -m 200 '[90,120]' ;
-sleep 1 ;
-./../rtspin -w -v -p 0 -r 100 -H '100,101' 40 200 1 &
-sleep 1 ;
-./../release_ts ;
