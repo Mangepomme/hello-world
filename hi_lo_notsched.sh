877b64b21ff7849d8be99b7d3799498bce2e3bb0
Louise Flick <louise.flick@telecom-paris.fr>
2020-07-23 11:49:08 +0200

    delete trash

--- hi_lo_notsched.sh
+++
@@ -1,8 +0,0 @@
-./../setsched G-RES ;
-./../resctl -n 100 -c 1 -t table-driven -m 200 '[10,30]' ;
-./../resctl -n 101 -c 0 -t table-driven -m 200 '[90,190]' ;
-./../resctl -n 102 -c 0 -t table-driven -m 200 '[40,70]' ;
-./../rtspin -w -v -p 0 -r 100 -H '100,101' 20 200 1 &
-./../rtspin -w -v -p 0 -r 102 -z 4 30 200 1 &
-sleep 1 ;
-./../release_ts ;
--- lo_twolores.sh
+++
@@ -1,6 +0,0 @@
-./../setsched G-RES ;
-./../resctl -n 100 -c 1 -t table-driven -m 200 '[10,30]' ;
-./../resctl -n 100 -c 0 -t table-driven -m 200 '[40,70]' ;
-./../rtspin -w -v -p 0 -r 100 -z 4 40 200 1 &
-sleep 1 ;
-./../release_ts ;
